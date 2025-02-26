46b30fd0c9cd28f7684d5b73171799c00d5e4e04
ulieth <uliana.eth@proton.me>
2025-02-26 18:10:16 +0300

    testing deposit 0 ETH possibility in exchange of 0 WETH

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Invariant Testing WETH With Foundry
 ### Invariant tests from the ground up for Wrapped Ether, one of the most important contracts on mainnet.
+
+What is fuzzing?
+Generate a random input -> Test something -> If it fails, save input (or stop)
+assertions violations
+1) Identify your invariants/system properties in plain English
+2) Convert your properties to code
+
+
+Tests should have precondition, action, postcondition
--- test/WETH9.invariants.t.sol
+++ test/WETH9.invariants.t.sol
@@ -5,7 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {WETH9} from "../src/WETH9.sol";
 
-contract WETH9Invariants is StdInvariant, Test  {
+contract WETH9Invariants is StdInvariant, Test {
     WETH9 public weth;
 
     function setUp() public {
@@ -13,6 +13,12 @@ contract WETH9Invariants is StdInvariant, Test  {
     }
 
     function invariant_wethSupplyIsAlwaysZero() public {
+        assertEq(0, weth.totalSupply());
+    }
+
+    function test_zeroDeposit() public {
+        weth.deposit{value: 0}();
+        assertEq(0, weth.balanceOf(address(this)));
         assertEq(0, weth.totalSupply());
     }
 }
